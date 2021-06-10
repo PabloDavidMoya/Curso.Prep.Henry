db52ac425648daf7944b1bb9568a3973eadbf56d
PabloDavidMoya <pablodavid2007@gmail.com>
2021-06-10 16:34:18 -0300

    02-JS

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,16 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var nuevoObj = 
+  {
+     nombre: nombre,
+     edad: edad,
+     meow: function()
+     {
+       return "Meow!"
+     }
+  }  
+  return nuevoObj;
 }
 
 
@@ -14,6 +24,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property]=null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
